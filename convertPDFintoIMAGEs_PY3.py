386921c8482cd81eaf3df773bc7e2198f77cce5a
nunobbrandao <nunobandarrinhabrandao@gmail.com>
2020-09-21 10:14:37 +0100

    Format excel to a calc sheet & Py to extract pdf into jpeg

---
+++ convertPDFintoIMAGEs_PY3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from pdf2image import convert_from_path
+import glob
+import os
+
+#list of files to be imported
+files = glob.glob(os.path.join(os.getcwd(),'Qrt_No 9 - App*.pdf')) 
+
+for file in files:
+    file_name = file.split("\\")[-1].split(".")[0]
+    pages = convert_from_path(file, 500)
+    for i,page in enumerate(pages):
+        page.save(os.path.join(os.getcwd(), file_name + "_{}.jpg".format(i)), 'JPEG')
+    
