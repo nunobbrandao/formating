3f7f58f4f7bc5bfe6638e924cf19c8d8ba16015f
nunobbrandao <nunobandarrinhabrandao@gmail.com>
2020-10-01 15:13:32 +0100

    bug fix on excel formating

--- convertPDFintoIMAGEs_PY3.py
+++ convertPDFintoIMAGEs_PY3.py
@@ -9,11 +9,11 @@ import glob
 import os
 
 #list of files to be imported
-files = glob.glob(os.path.join(os.getcwd(),'Qrt_No 9 - App*.pdf')) 
+files = glob.glob(os.path.join(os.getcwd(),'*.pdf')) 
 
 for file in files:
-    file_name = file.split("\\")[-1].split(".")[0]
+    file_name = file.split("\\")[-2] + "_" + file.split("\\")[-1].split(".")[0]
     pages = convert_from_path(file, 500)
     for i,page in enumerate(pages):
         page.save(os.path.join(os.getcwd(), file_name + "_{}.jpg".format(i)), 'JPEG')
-    
+print("Code Run")
